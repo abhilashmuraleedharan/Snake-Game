af95a55a5650b4de4f21782c7097e206d09ea26c
Abhilash Muraleedharan <abhilashmuraleedharan@outlook.com>
2020-04-26 00:29:49 +0530

    feat: Add custom features to the game
    Following features are added to the game
    * Get the player name before the game begins.
    * If the person is new, introduce the player to game controls
    * If the player name is already present, check with player whether he/she is an old player and wants to improve his/her score OR if the is a new player then allow the player to come up with a different name
    * Congratulate the player if he is the top scorer
    * Add leaderboard feature

--- src/game.h
+++ src/game.h
@@ -2,6 +2,10 @@
 #define GAME_H
 
 #include <random>
+#include <string>
+#include <unordered_map>
+#include <thread>
+#include <future>
 #include "SDL.h"
 #include "controller.h"
 #include "renderer.h"
@@ -14,31 +18,50 @@ class Game {
        Controller &&controller, Renderer &&renderer);
 
   // Public Methods
-  void run(const std::size_t &targetFrameDuration);
-  void showGameBanner();
-
+  void displayScoreBoard();
+  void run();
+  
   // Getters
   int getScore() const;
-  int getSize() const;
+  int getHighScore() const;
+  std::string getPlayerName() const;
+
+  // Public Data
+  const std::string kScoreBoardPath{"../assets/scoreboard.txt"};
+  const std::size_t kFramesPerSecond{60};
+  const std::size_t kTargetFrameDuration{1000 / kFramesPerSecond};
 
  private:
 
   // Private methods
   void placeFood_();
   void update_(bool &running);
+  bool newPlayer_(std::string name);
+  void updateScoreBoard_();
+  void showGameBanner_();
+  void getPlayerDetails_();
+  void readScoreBoard_();
+  void run_();
+  void displayResult_();
 
   // Private data
-  Snake      _snake;
-  Controller _gController;
-  Renderer   _gRenderer;
-  SDL_Point  _food;
-  int        _score{0};
+  Snake        _snake;
+  Controller   _gController;
+  Renderer     _gRenderer;
+  SDL_Point    _food;
+  int          _score{0};
+  int          _highScore{0};
+  std::string  _playerName{};
+  std::string  _topScorer{};
 
+  // For randomly placing food 
   std::random_device _dev;
   std::mt19937    _engine;
   std::uniform_int_distribution<int> _randomW;
   std::uniform_int_distribution<int> _randomH;
 
+  // To store players and their scores
+  std::unordered_map <std::string, std::string> _scoreboard{};
 };
 
 #endif
