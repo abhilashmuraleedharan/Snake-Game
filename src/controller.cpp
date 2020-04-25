6eed220eecd2ef1afa8b6c4a2a8ebfa678348c5f
Abhilash Muraleedharan <abhilashmuraleedharan@outlook.com>
2020-04-25 17:35:41 +0530

    feat: Add support for extra keyboard controls
    Add support for following key options
    * ‘w’ ‘a’ ’s’ ‘d’ support along with arrow keys
    * press ‘q’ to quit the game in the middle

--- src/controller.cpp
+++ src/controller.cpp
@@ -17,23 +17,31 @@ void Controller::handleInput(bool &running, Snake &snake) const {
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
+        case SDLK_w:
           changeDirection_(snake, Snake::Direction::kUp,
                            Snake::Direction::kDown);
           break;
 
         case SDLK_DOWN:
+        case SDLK_s:
           changeDirection_(snake, Snake::Direction::kDown,
                            Snake::Direction::kUp);
           break;
 
         case SDLK_LEFT:
+        case SDLK_a:
           changeDirection_(snake, Snake::Direction::kLeft,
                            Snake::Direction::kRight);
           break;
 
         case SDLK_RIGHT:
+        case SDLK_d:
           changeDirection_(snake, Snake::Direction::kRight,
                            Snake::Direction::kLeft);
+          break;
+
+        case SDLK_q:
+          running = false;
           break;
       }
     }
